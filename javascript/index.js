9ef9c45264764c1bc809e48f800157db27069f83
omarfaruk <omarabdullah1811@gmail.com>
2022-02-17 18:31:49 +0600

    handle all error & Finish work

--- javascript/index.js
+++ javascript/index.js
@@ -1,74 +1,73 @@
 // get inputValue 
-function getInputValue(inputId){
+function getInputValue(inputId) {
     const inputFild = document.getElementById(inputId)
     const inputText = inputFild.value;
     inputValue = parseInt(inputText);
     return inputValue;
 }
 // set total value in html 
-function updateHtml(elementId, setvalue){
+function updateHtml(elementId, setvalue) {
     document.getElementById(elementId).innerText = setvalue;
 }
 // get error with Interesting way 
-function getError(inputFildName){
+function getError(inputFildName) {
     document.getElementById('displayError').classList.remove('d-none');
-    document.getElementById('inputName').innerText = inputFildName; 
+    document.getElementById('inputName').innerText = inputFildName;
 }
 // calculat expenses part 
-document.getElementById('calculateButton').addEventListener('click',function(){
+document.getElementById('calculateButton').addEventListener('click', function () {
     const income = getInputValue('incomeFild');
     const food = getInputValue('foodFild');
     const rent = getInputValue('rentFild');
     const clothes = getInputValue('clothesFild');
-    if(isNaN(income) || income < 0){
+    // Handle error 
+    if (isNaN(income) || income < 0) {
         getError('income value');
-    }
-    else if(isNaN(food) || food < 0){
+    } else if (isNaN(food) || food < 0) {
         getError('food value');
-    }
-    else if(isNaN(rent) || rent < 0){
+    } else if (isNaN(rent) || rent < 0) {
         getError('rent value');
-    }
-    else if(isNaN(clothes) || clothes < 0){
+    } else if (isNaN(clothes) || clothes < 0) {
         getError('clothes value ');
-    }
-    else{
-    document.getElementById('displayError').classList.add('d-none');
-    document.getElementById('displayErrorTwo').classList.add('d-none');
-    const totalExpenses = food + rent + clothes;
-    const balance = income - totalExpenses;
-    if(income > totalExpenses){
-    // update on the html doc 
-    updateHtml('totalExpenses',totalExpenses)
-    updateHtml('balance',balance)
-    }
-    else{
-       document.getElementById('displayErrorTwo').classList.remove('d-none');
-       updateHtml('totalExpenses',totalExpenses)
-       updateHtml('balance', 00)
-    }
+    } else {
+        document.getElementById('displayError').classList.add('d-none');
+        document.getElementById('displayErrorTwo').classList.add('d-none');
+        const totalExpenses = food + rent + clothes;
+        const balance = income - totalExpenses;
+        if (income > totalExpenses) {
+            // update on the html doc 
+            updateHtml('totalExpenses', totalExpenses)
+            updateHtml('balance', balance)
+        } else {
+            // Handle error
+            document.getElementById('displayErrorTwo').classList.remove('d-none');
+            updateHtml('totalExpenses', totalExpenses)
+            updateHtml('balance', 00)
+        }
     }
 })
 
-document.getElementById('saveButton').addEventListener('click',function(){
+document.getElementById('saveButton').addEventListener('click', function () {
     const balanceText = document.getElementById('balance').innerText;
     const balance = parseInt(balanceText)
     const income = getInputValue('incomeFild');
     const save = getInputValue('saveFild');
-    if(isNaN(save) || save < 0){
-        getError('save value')
-    }
-    else{
-    const saveAmount =(save / 100) * income;
-    const remainingBalance = balance - saveAmount;
-    // update on the html doc 
-    if(saveAmount < balance){
-    updateHtml('savingAmount',saveAmount);
-    updateHtml('remainingBalance',remainingBalance)
-    }
-    else{
-
-    }
+    document.getElementById('displayErrorThree').classList.add('d-none');
+    if (isNaN(save) || save < 0) {
+        document.getElementById('displayErrorThree').classList.remove('d-none');
+    } else {
+        const saveAmount = parseInt((save / 100) * income);
+        const remainingBalance = balance - saveAmount;
+        if (saveAmount <= balance) {
+            // update on the html doc 
+            updateHtml('savingAmount', saveAmount);
+            updateHtml('remainingBalance', remainingBalance);
+            document.getElementById('displayErrorFour').classList.add('d-none');
+        } else {
+            // Handle error
+            document.getElementById('displayErrorFour').classList.remove('d-none');
+            updateHtml('savingAmount', 00);
+            updateHtml('remainingBalance', 00);
+        }
     }
 })
-
